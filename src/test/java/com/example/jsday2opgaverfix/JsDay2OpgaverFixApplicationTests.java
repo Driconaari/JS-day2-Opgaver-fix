4021698065d2e973c57b3481908783d02b8e33b5
Asger <136756772+Driconaari@users.noreply.github.com>
2024-09-13 13:51:48 +0200

    Initial commit

---
+++ src/test/java/com/example/jsday2opgaverfix/JsDay2OpgaverFixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsday2opgaverfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsDay2OpgaverFixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
